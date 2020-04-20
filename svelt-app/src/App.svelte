2c0941f4aa71a96de74ec14f7540ccf7246a0b74
Paul Millen <pwmillen@gmail.com>
2020-04-20 18:45:03 +0100

    using svelte now

---
+++ svelt-app/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { Router, Route } from "svelte-routing";	
+	import { Link } from "svelte-routing";
+	import Home from './routes/Home.svelte'
+	import Conjugator from './routes/Conjugator.svelte'
+</script>
+
+<Router>
+  <nav>
+    <Link to='/'>Home</Link>
+    <Link to='conjugator'>Conjugator</Link>
+  </nav>
+  <div>
+    <Route path="/conjugator" component={Conjugator} />
+    <Route path="/" component={Home} />
+  </div>
+</Router>
+
+<style>
+</style>
