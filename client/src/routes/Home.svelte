bcdd4daa4b4b26797562ecbc7d5bada98320964b
Paul Millen <pwmillen@gmail.com>
2020-06-02 16:53:28 +0100

    added sphere onclick behaviour

--- client/src/routes/Home.svelte
+++ client/src/routes/Home.svelte
@@ -2,23 +2,32 @@
   import { onMount } from "svelte";
   import { Link } from "svelte-routing";
   import * as THREE from "three";
+  import TWEEN from "@tweenjs/tween.js";
 
   onMount(() => {
     init();
     animate();
   });
 
+  const mouse = new THREE.Vector2();
   let renderer,
     scene,
     camera,
+    raycaster,
     baseCentre,
     baseRight,
     baseLeft,
     sphereCentre,
     sphereRight,
-    sphereLeft;
+    sphereLeft,
+    intersected,
+    selectedSphere,
+    tween;
 
   function init() {
+    window.addEventListener("mousemove", updateMouseCoords, false);
+    window.addEventListener("click", handleMouseClick, true);
+
     camera = new THREE.PerspectiveCamera(
       70,
       window.innerWidth / window.innerHeight,
@@ -26,6 +35,8 @@
       1000
     );
     camera.position.set(0, 0.4, 1.3);
+
+    raycaster = new THREE.Raycaster();
 
     const loader = new THREE.TextureLoader();
     const shadowTexture = loader.load("/assets/roundshadow.png");
@@ -59,36 +70,42 @@
     shadowMeshCentre.rotation.x = Math.PI * -0.5;
     shadowMeshCentre.scale.set(0.3, 0.3, 0.3);
 
-    sphereCentre = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphereCentre = new THREE.Mesh(sphereGeometry, sphereMaterial.clone());
     sphereCentre.position.set(0, 0.5, 0);
+    sphereCentre.name = "sphereCentre";
     baseCentre.add(sphereCentre);
     baseCentre.add(shadowMeshCentre);
 
     baseRight = new THREE.Object3D();
     scene.add(baseRight);
 
-    const shadowMeshRight = new THREE.Mesh(shadowGeometry, shadowMaterial);
+    const shadowMeshRight = shadowMeshCentre.clone();
     shadowMeshRight.position.set(0.63, 0.001, -0.1);
     shadowMeshRight.rotation.x = Math.PI * -0.5;
     shadowMeshRight.rotation.y = -0.06;
     shadowMeshRight.scale.set(0.3, 0.3, 0.3);
 
-    sphereRight = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphereRight = new THREE.Mesh(sphereGeometry, sphereMaterial.clone());
     sphereRight.position.set(0.6, 0.5, 0);
+    sphereRight.name = "sphereRight";
     baseRight.add(shadowMeshRight);
     baseRight.add(sphereRight);
 
     baseLeft = new THREE.Object3D();
     scene.add(baseLeft);
 
-    const shadowMeshLeft = new THREE.Mesh(shadowGeometry, shadowMaterial);
+    const shadowMeshLeft = new THREE.Mesh(
+      shadowGeometry,
+      shadowMaterial.clone()
+    );
     shadowMeshLeft.position.set(-0.63, 0.001, -0.1);
     shadowMeshLeft.rotation.x = Math.PI * -0.5;
     shadowMeshLeft.rotation.y = 0.06;
     shadowMeshLeft.scale.set(0.3, 0.3, 0.3);
 
-    sphereLeft = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphereLeft = new THREE.Mesh(sphereGeometry, sphereMaterial.clone());
     sphereLeft.position.set(-0.6, 0.5, 0);
+    sphereLeft.name = "sphereLeft";
     baseLeft.add(shadowMeshLeft);
     baseLeft.add(sphereLeft);
 
@@ -99,18 +116,15 @@
     threeContainer.appendChild(renderer.domElement);
   }
 
-  window.addEventListener("mousemove", mouseMove, false);
-  const mouse = new THREE.Vector2();
-
-  function mouseMove(event) {
+  function updateMouseCoords(event) {
     event.preventDefault();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
   }
 
-  function render() {
-    var raycaster = new THREE.Raycaster();
-    var intersects = [];
+  function handleSphereOnHover(time) {
+    let intersects = [];
+    const intersectedOrigColor = 0xffffff;
 
     raycaster.setFromCamera(mouse, camera);
     intersects = raycaster.intersectObjects([
@@ -119,27 +133,130 @@
       sphereLeft
     ]);
 
-    if (intersects.length !== 0) {
-      intersects[0].object.parent.position.z = 0.2;
-    } else if (intersects !== null) {
-      baseCentre.position.z = 0;
-      baseRight.position.z = 0;
-      baseLeft.position.z = 0;
-      intersects = null;
-    }
+    if (intersects.length) {
+      if (intersected !== intersects[0].object) {
+        intersected = intersects[0].object;
 
+        if (intersected.name === "sphereLeft") {
+          tween = new TWEEN.Tween(intersected.material.color)
+            .to({ r: 1, g: -2, b: -2 }, 500)
+            .easing(TWEEN.Easing.Quartic.In)
+            .start();
+        } else if (intersected.name === "sphereCentre") {
+          tween = new TWEEN.Tween(intersected.material.color)
+            .to({ r: -2, g: 1, b: -2 }, 500)
+            .easing(TWEEN.Easing.Quartic.In)
+            .start();
+        } else if (intersected.name === "sphereRight") {
+          tween = new TWEEN.Tween(intersected.material.color)
+            .to({ r: -2, g: -2, b: 1 }, 500)
+            .easing(TWEEN.Easing.Quartic.In)
+            .start();
+        }
+      }
+    } else {
+      if (intersected) {
+        intersected.material.color.setHex(0xffffff);
+      }
+      intersected = null;
+    }
+  }
+
+  function handleMouseClick(event) {
+    updateMouseCoords(event);
+
+    let intersects = [];
+
+    raycaster.setFromCamera(mouse, camera);
+    intersects = raycaster.intersectObjects(
+      [sphereCentre, sphereRight, sphereLeft],
+      true
+    );
+
+    if (intersects.length) {
+      if (selectedSphere && selectedSphere !== intersects[0].object) {
+        tween = new TWEEN.Tween(selectedSphere.parent.position)
+          .to(
+            {
+              x: selectedSphere.originalParentPosition.x,
+              y: selectedSphere.originalParentPosition.y,
+              z: selectedSphere.originalParentPosition.z
+            },
+            2500
+          )
+          .easing(TWEEN.Easing.Elastic.Out)
+          .start();
+      } else if (selectedSphere) {
+        tween = new TWEEN.Tween(selectedSphere.parent.position)
+          .to(
+            {
+              x: selectedSphere.originalParentPosition.x,
+              y: selectedSphere.originalParentPosition.y,
+              z: selectedSphere.originalParentPosition.z
+            },
+            2500
+          )
+          .easing(TWEEN.Easing.Elastic.Out)
+          .start();
+
+        selectedSphere = null;
+        return;
+      }
+
+      selectedSphere = intersects[0].object;
+      selectedSphere.originalParentPosition = selectedSphere.parent.position.clone();
+      tween = new TWEEN.Tween(selectedSphere.parent.position)
+        .to(
+          {
+            x: -selectedSphere.position.x,
+            y: -0.05,
+            z: 1
+          },
+          2500
+        )
+        .easing(TWEEN.Easing.Elastic.Out)
+        .start();
+    } else if (selectedSphere) {
+      tween = new TWEEN.Tween(selectedSphere.parent.position)
+        .to(
+          {
+            x: selectedSphere.originalParentPosition.x,
+            y: selectedSphere.originalParentPosition.y,
+            z: selectedSphere.originalParentPosition.z
+          },
+          2500
+        )
+        .easing(TWEEN.Easing.Elastic.Out)
+        .start();
+
+      selectedSphere = null;
+    }
+  }
+
+  function sphereMovement(time) {
+    time *= 0.0004;
+    const yOffOne = -Math.abs(Math.sin(time * 2 - 0.06));
+    const yOffTwo = Math.abs(Math.sin(time * 2 - 0.004));
+    const yOffThree = Math.abs(Math.sin(time * 2 + 0.007)) * 0.9;
+    sphereCentre.position.y =
+      0.5 + THREE.MathUtils.lerp(-0.009, 0.012, yOffOne);
+    sphereRight.position.y = 0.5 + THREE.MathUtils.lerp(-0.01, 0.009, yOffTwo);
+    sphereLeft.position.y = 0.48 + THREE.MathUtils.lerp(-0.01, 0.01, yOffThree);
+  }
+
+  function update(time) {
+    handleSphereOnHover(time);
+    sphereMovement(time);
+  }
+
+  function render() {
     renderer.render(scene, camera);
   }
 
   function animate(time) {
-    time *= 0.0007;
-    const yOffOne = Math.abs(Math.sin(time * 2 - 0.03));
-    const yOffTwo = Math.abs(Math.sin(time * 2 - 0.004));
-    const yOffThree = Math.abs(Math.sin(time * 2 + 0.003));
-    sphereCentre.position.y = 0.5 + THREE.MathUtils.lerp(-0.01, 0.013, yOffOne);
-    sphereRight.position.y = 0.5 + THREE.MathUtils.lerp(-0.01, 0.009, yOffTwo);
-    sphereLeft.position.y = 0.5 + THREE.MathUtils.lerp(-0.01, 0.01, yOffThree);
     requestAnimationFrame(animate);
+    TWEEN.update();
+    update(time);
     render();
   }
 </script>
